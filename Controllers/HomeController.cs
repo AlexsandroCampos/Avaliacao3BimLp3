d7418996062215bbabcbd731785863c6eba5e5ad
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-09-18 09:24:12 -0300

    Criando a página do site com a listagem de lojas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-using Microsoft.AspNetCore.Mvc;
-using Avaliacao3BimLp3.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 using Avaliacao3BimLp3.ViewModels;
 
 namespace Avaliacao3BimLp3.Controllers;
@@ -8,11 +6,11 @@ namespace Avaliacao3BimLp3.Controllers;
 public class HomeController : Controller
 {
     private static List<LojaViewModel> lojas = new List<LojaViewModel>{
-        new LojaViewModel(1, "piso 3", "Tênis Brasil", "Aqui você encontra os tênis", false, "tenis@email.com"),
-        new LojaViewModel(2, "piso 5", "Lembranças Já", "Venha comprar sua lembrança", true, "lemb@email.com"),
+        new LojaViewModel(1, "Tênis Brasil", "Piso 3", "Aqui você encontra os tênis", false, "tenis@email.com"),
+        new LojaViewModel(2, "Lembranças Já", "Piso 5", "Venha comprar sua lembrança", true, "lemb@email.com"),
     };
 
-    public IActionResult Index() => View();
+    public IActionResult Index() => View(lojas);
 
     public IActionResult Privacy() => View();
 }
