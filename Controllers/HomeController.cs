a25ac27e84ed790c2a061e0fea32b12855ea3e46
AlexsandroCampos <alexsandro.campos@aluno.ifsp.edu.br>
2022-09-24 16:59:53 -0300

    Alterando o nome das actions

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -7,38 +7,54 @@ namespace Avaliacao3BimLp3.Controllers;
 public class HomeController : Controller
 {
     private static List<EstabelecimentoViewModel> estabelecimentos = new List<EstabelecimentoViewModel>{
-        new EstabelecimentoViewModel(1, "Tênis Brasil", "piso 3", "Aqui você encontra os tênis", LojaEnum.Loja, "tenis@email.com"),
-        new EstabelecimentoViewModel(2, "Lembranças Já", "piso 1", "Venha comprar sua lembrança", LojaEnum.Kiosque, "lemb@email.com"),
+        new EstabelecimentoViewModel(1, "Tênis Brasil", "Piso 3", "Aqui você encontra os tênis", LojaEnum.Loja, "tenis@email.com"),
+        new EstabelecimentoViewModel(2, "Lembranças Já", "Piso 1", "Venha comprar sua lembrança", LojaEnum.Kiosque, "lemb@email.com"),
     };
 
     public IActionResult Index() => View(estabelecimentos);
-    public IActionResult IndexAdmin() => View(estabelecimentos);
-    public IActionResult Show(int id) => View(estabelecimentos.Find(l => l.Id == id));
-    public IActionResult Delete(int id)
+    public IActionResult AdminIndex() => View(estabelecimentos);
+    public IActionResult AdminShow(int id) => View(estabelecimentos.Find(l => l.Id == id));
+    public IActionResult AdminDelete(int id)
     {
         var loja = estabelecimentos.Find(l => l.Id == id);
+
         if(loja  == null) {
-            return RedirectToAction("IndexAdmin");
+            return RedirectToAction("AdminIndex");
         }
+
         estabelecimentos.Remove(loja);
-        return RedirectToAction("IndexAdmin");
+        return RedirectToAction("AdminIndex");
     } 
-    public IActionResult Create() => View();
+    public IActionResult AdminCreate()
+    {
+        return View();
+
+    }
+
 
     [HttpPost]
-    public IActionResult Create(EstabelecimentoViewModel estabelecimentoViewModel) 
+    public IActionResult AdminCreate(EstabelecimentoViewModel estabelecimentoViewModel) 
     {
         foreach (var estabelecimento in estabelecimentos)
         {
-            if(estabelecimento.Nome.Equals(estabelecimentoViewModel.Nome)) {
-            ViewBag.MensagemErro = "Nome de loja já existente";
-            return View();
+            if(estabelecimento.Nome.Equals(estabelecimentoViewModel.Nome)) 
+            {
+                ViewBag.MensagemErro = "Nome de loja já existente";
+                return View("AdminCreate");
+            }
         }
+
+        if(estabelecimentos.Count > 0) 
+        {
+            estabelecimentoViewModel.Id =  estabelecimentos.Last().Id + 1;
         }
         
-        estabelecimentoViewModel.Id = estabelecimentos.Last().Id + 1;
+        else 
+        {
+            estabelecimentoViewModel.Id = 1;
+        }
         
         estabelecimentos.Add(estabelecimentoViewModel);
-        return RedirectToAction("Show", new {id = estabelecimentoViewModel.Id});
+        return RedirectToAction("AdminShow", new {id = estabelecimentoViewModel.Id});
     }
 }
